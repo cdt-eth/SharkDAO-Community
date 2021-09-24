887a4694a2d939673330746598b7130a8cffdb5d
christianturner <christianturner@hey.com>
2021-09-24 14:05:51 -0400

    remove font imports from Header

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -8,19 +8,6 @@ const Header = () => {
       <meta name="SharkDAO" content="SharkDAO.Community" />
       {/* favicon */}
       <link rel="icon" href="/shark-fav-black.ico" />
-      {/* custom fonts */}
-      <link
-        rel="preload"
-        href="MsGothic-SharkDAOMembersOnly-Regular.woff"
-        as="font"
-        type="font/woff"
-      />
-      <link
-        rel="preload"
-        href="MsGothic-SharkDAOMembersOnly-Heavy.woff"
-        as="font"
-        type="font/woff"
-      />
     </Head>
   );
 };
