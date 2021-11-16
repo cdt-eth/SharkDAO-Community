630ad634cbc18a7c35e85294eb98faf93259a3c5
rayo <0xrayo.eth@gmail.com>
2021-11-16 13:28:50 -0600

    remove NFTs from display when disconnected

--- pages/components/Mint.tsx
+++ pages/components/Mint.tsx
@@ -4,7 +4,6 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { useWeb3React } from "@web3-react/core";
 import { ApprovedMintsRes } from "../api/approved";
-import {ethers} from 'ethers';
 
 const injected = new InjectedConnector({
   supportedChainIds: [1, 4]
@@ -19,8 +18,6 @@ const walletconnect = new WalletConnectConnector({
 const Mint = () => {
 
   const [eligbleNfts, setEligbleNfts] = useState<Number[]>([]);
-  const [checkedEligible, setCheckedEligible] = useState<boolean>(false);
-  
   const { active, account, activate, deactivate } = useWeb3React();
 
   const connectMM = async () => {
@@ -42,7 +39,6 @@ const Mint = () => {
   const disconnect = async () => {
     try {
       await deactivate()
-      setCheckedEligible(false);
     } catch (err) {
       console.log(err);
       
@@ -62,12 +58,27 @@ const Mint = () => {
     });
     const approved: ApprovedMintsRes  = await resApproved.json();
     setEligbleNfts(approved['ids']);
-    setCheckedEligible(true);
   }, [account]);
   
+  const disconnectedContent = () => {
+    return(
+      <>
+      </>
+    );
+  }
+
+  const connectedContent = () => {
+    return (
+      eligbleNfts.length == 0 ? 
+          <h3>You are not elibile for any mints, but you may be eligible for other mints in the future</h3> :
+          <>
+          {eligbleNfts.map(id => <CommNFT key={id as number} id={id}/>)}
+          </>
+    );
+  }
+
   useEffect(() => {
     if(account) {
-      console.log("useEffect");
       getMintsRes();
     }
   }, [account, getMintsRes]);
@@ -76,14 +87,8 @@ const Mint = () => {
       {!active && <button onClick={connectMM}>CONNENCT WITH MM</button>}
       {!active && <button onClick={connectWC}>CONNENCT WALLET CONNECT</button>}
       {active && account}
-      {active && <button onClick={disconnect}>DISCONNECT</button>}
-      { checkedEligible &&
-          eligbleNfts.length == 0 ? 
-          <h3>You are not elibile for any mints, but you may be eligible for other mints in the future</h3> :
-          <>
-          {eligbleNfts.map(id => <CommNFT key={id as number} id={id}/>)}
-          </>
-      }
+      {active && <button onClick={disconnect}>DISCONNECT</button>}      
+      {active ? connectedContent() : disconnectedContent()}
     </div>
     )
 }
