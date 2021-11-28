c42f4b2eb47a2acadd67d3791e10269e8f89ea8f
christianturner <christianturner@hey.com>
2021-11-28 05:55:13 -0500

    rayo changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Web3ReactProvider } from "@web3-react/core";
-import { ethers } from 'ethers';
-
+import { ethers } from "ethers";
 
 function getLibrary(provider: any) {
   const library = new ethers.providers.Web3Provider(provider);
@@ -10,10 +9,10 @@ function getLibrary(provider: any) {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <Component {...pageProps} />
-  </Web3ReactProvider>
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
   );
 }
 export default MyApp;
--- pages/api/approved.ts
+++ pages/api/approved.ts
@@ -1,16 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-const whitelistData = require('./whitelist.json');
-
+import type { NextApiRequest, NextApiResponse } from "next";
+const whitelistData = require("./whitelist.json");
 
 export type ApprovedMintsRes = {
-  ids: Array<Number>
-}
+  ids: Array<Number>;
+};
 
 type WhitelistData = {
-  id: Number,
-  leaves: string[]
-}
+  id: Number;
+  leaves: string[];
+};
 
 export default function getApprovedMints(
   req: NextApiRequest,
@@ -23,16 +22,14 @@ export default function getApprovedMints(
     const lowerAddress = address.toLowerCase();
 
     let approved: Array<Number> = [];
-    const merkleLeaves: WhitelistData[] = whitelistData['whitelist'];
+    const merkleLeaves: WhitelistData[] = whitelistData["whitelist"];
     merkleLeaves.forEach((element) => {
-
-      element['leaves'].forEach((leaf) => {
+      element["leaves"].forEach((leaf) => {
         if (leaf === lowerAddress) {
-          approved.push(element['id']);
+          approved.push(element["id"]);
         }
       });
     });
-    res.status(200).json({ ids: approved })
+    res.status(200).json({ ids: approved });
   }
 }
-
